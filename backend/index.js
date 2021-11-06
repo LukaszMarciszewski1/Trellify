e4c572b41df0424f9d0a9718b06169307aebf99b
Łukasz Marciszewski <lukasz.marciszewski@appfinity.pl>
2021-11-06 13:49:32 +0100

    prepare to panel tasks

--- backend/index.js
+++ backend/index.js
@@ -27,9 +27,16 @@ mongoose
     console.log(`${error} did not connect`)
   })
 
-app.get('/', (req, res) => {
-  res.send('server is working')
-})
+// app.get('/', (req, res) => {
+//   res.send('server is working')
+// })
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.listen(process.env.PORT, () => {
   console.log('server słucha')
