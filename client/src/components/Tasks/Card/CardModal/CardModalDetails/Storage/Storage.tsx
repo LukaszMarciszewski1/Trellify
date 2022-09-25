d729dc237525ee075325bec9b7f1d3f01d99b064
Łukasz Marciszewski <lmarciszewski@gravityglobal.com>
2022-09-25 14:09:55 +0200

    products list logic for storage card window

--- client/src/components/Tasks/Card/CardModal/CardModalDetails/Storage/Storage.tsx
+++ client/src/components/Tasks/Card/CardModal/CardModalDetails/Storage/Storage.tsx
@@ -7,20 +7,22 @@ import {
 } from "../../../../../../store/api/products"
 import { Product as ProductModel } from '../../../../../../models/product';
 import Button from '../../../../../Details/Button/Button'
+import IconButton from '../../../../../Details/IconButton/IconButton';
 
 const Storage: React.FC = () => {
   const { data, error, isLoading } = useGetAllProductsQuery()
-  const [productList, setProductList] = useState<string[]>([])
   const [selectProduct, setSelectProduct] = useState<string>('')
-  const [list, setList] = useState<any>()
+  const [productsIdList, setProductsIdList] = useState<string[]>([])
+  const [products, setProducts] = useState<ProductModel[] | undefined>()
 
   useEffect(() => {
     if (!data) return
-    const firstElement = [...data].map(product => product.name)
-    setSelectProduct(firstElement[0])
-    // setList(displayProductFromList())
-  }, [data, productList])
+    setSelectProduct(data[0]._id)
+  }, [data])
 
+  useEffect(() => {
+    displayProductFromList()
+  }, [productsIdList])
 
   const handleSelectProduct = (e: React.ChangeEvent<HTMLSelectElement>) => {
     if (!data) return
@@ -28,55 +30,69 @@ const Storage: React.FC = () => {
   }
 
   const handleAddProductToList = () => {
-    if (!selectProduct) return
-    setProductList([...productList, selectProduct])
-    console.log(list)
-  }
-
-  const displayProductFromList = (data: ProductModel[] | undefined) => {
     if (!data) return
-    return [...data].filter(p1 => productList.some(p2 => p1._id === p2))
+    setProductsIdList([...productsIdList, selectProduct])
+
   }
 
-  // console.log(selectProduct)
-  // console.log(productList)
-  // console.log(displayProductFromList())
+  const displayProductFromList = () => {
+    if (!data) return
+    const products = [...data].filter(p1 => productsIdList.some(p2 => p1._id === p2))
+    setProducts(products)
+  }
 
+  const handleRemoveFromList = (id: string) => {
+    if (!products) return
+    const newProducts = [...products].filter(item => item._id !== id)
+    setProducts(newProducts)
+  }
 
   return (
     <div className={styles.container}>
-      <div className={styles.formContainer}>
-        <form>
-          <label htmlFor="products">Dodaj z magazynu</label>
-          <div className={styles.inputContainer}>
-            <select className={styles.select} onChange={(e) => handleSelectProduct(e)}>
-              {
-                data ? (
-                  data.map(product => (
-                    <option key={product._id} value={product._id}>{product.name}</option>
-                  ))
-                ) : null
-              }
-            </select>
-            <Button
-              onClick={handleAddProductToList}
-              title={'+'} type={'button'}
-              style={{
-                height: '35px',
-                width: '35px',
-                marginLeft: '10px'
-              }} />
-          </div>
-        </form>
-      </div>
+      <form className={styles.form}>
+        <label htmlFor="products">Dodaj z magazynu</label>
+        <div className={styles.inputContainer}>
+          <select className={styles.select} onChange={(e) => handleSelectProduct(e)}>
+            {
+              data ? (
+                data.map(product => (
+                  <option key={product._id} value={product._id}>{product.name}</option>
+                ))
+              ) : null
+            }
+          </select>
+          <Button
+            onClick={handleAddProductToList}
+            title={'Dodaj'} type={'button'}
+            style={{
+              height: '35px',
+              // width: '100px',
+              marginLeft: '10px'
+            }} />
+        </div>
+      </form>
       <div className={styles.list}>
-        {
-          displayProductFromList(data)?.map(item => (
-            <div key={item._id}>
-              <span>{item.name}</span>
-            </div>
-          ))
-        }
+        <form className={styles.form}>
+          {
+            products?.map(item => (
+              <div key={item._id}>
+                <span>{item.name}</span>
+                <input type="text" name="name" value={`stan: ${item.quantity} ${item.unit}`} disabled />
+                <input type="number" name="name" defaultValue={item.quantity} min={1} max={item.quantity} />
+                <IconButton onClick={() => handleRemoveFromList(item._id)} style={{ marginLeft: '10px' }}>X</IconButton>
+                {/* <Button
+                  onClick={handleAddProductToList}
+                  title={'X'} type={'button'}
+                  style={{
+                    backgroundColor: 'transparent',
+                    color: 'black',
+                    padding: '5px',
+                    marginLeft: '10px'
+                  }} /> */}
+              </div>
+            ))
+          }
+        </form>
       </div>
     </div>
   )
